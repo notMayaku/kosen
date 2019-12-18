905eb2805a6c44db5518233e6c6eed9f4c5fe741
mayakuzuki <57666915+notMayaku@users.noreply.github.com>
2019-12-18 17:58:25 +0900

    テスト済み

--- j4/jikken_kouki/searchDB.php
+++ j4/jikken_kouki/searchDB.php
@@ -1,27 +1,3 @@
-<?php
-   function DBsearch($searchWord){
-      try{
-         $server = "jocalc1";
-         $DB = "r01_j4_exp";
-         $portNumber = "5432";
-         $userID = "td16131";
-         $userPass = "td16131";
-
-         $dbh = new PDO("pgsql:host=$server; dbname=$DB; port=$portNumber; user=$userID; password=$userPass");
-         $sql = "SELECT * FROM company_21 WHERE name LIKE '?'";
-         $stmt = $dbh->prepare($sql);
-         $stmt->bindValue(1, $searchWord, PDO::PARAM_STR);
-         $stmt->execute();
-         $result = $stmt->fetch(PDO::FETCH_ASSOC);
-         $dbh = null;
-      }
-      catch(Exception $e){
-         return null;
-      }
-      return $result;
-   }
-?>
-
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -33,38 +9,77 @@
    <form method="post" action="searchDB.php">
       企業名部分一致検索：<input type="text" name="searchWord">
       <input type="submit" value="検索">
+      ソート:
+      <select name="sort">
+			<option value="ASC">昇順</option>
+			<option value="DESC">降順</option>
+      </select>
+      項目:
+      <select name="item">
+			<option value="code">企業コード</option>
+			<option value="name">企業名</option>
+			<option value="address">所在地</option>
+			<option value="phone">電話番号</option>
+			<option value="labors">従業員数</option>
+      </select>
    </form>
    <br>
    <?php
-      $searchWord = htmlspecialchars($_POST['searchWord'], ENT_QUOTES, 'UTF-8');
-      if($searchWord = ""){
-         echo "empty error";
-      }
-      else{
-         require_once("Company.php");
-         $result = DBsearch($searchWord);
-         $company = [];
-
-         foreach($result as $row){
-            $company[] = new Company($row['code'], $row['name'], $row['address'], $row['phone'], $row['labors']);
-         }
-
-         echo "企業数：" . htmlspecialchars(count($company), ENT_QUOTES, 'UTF-8') . "件\n<br>\n";
-         echo "<table>\n";
-         echo "<tr>\n";
-         echo "<th>企業コード</th><th>企業名</th><th>所在地</th><th>電話番号</th><th>従業員数</th>\n";
-         echo "<tr>\n";
-         foreach($company as $row){
-            echo "<tr>\n";
-            echo "<td>" . htmlspecialchars($row->getCode(), ENT_QUOTES, 'UTF-8') . "</td>\n";
-            echo "<td>" . htmlspecialchars($row->getName(), ENT_QUOTES, 'UTF-8') . "</td>\n";
-            echo "<td>" . htmlspecialchars($row->getAddress(), ENT_QUOTES, 'UTF-8') . "</td>\n";
-            echo "<td>" . htmlspecialchars($row->getPhone(), ENT_QUOTES, 'UTF-8') . "</td>\n";
-            echo "<td>" . htmlspecialchars($row->getLabors(), ENT_QUOTES, 'UTF-8') . "</td>\n";
-            echo "</tr>\n";
-         }
-         echo "</table>\n";
-      }
+		echo "<br>";
+		if(!empty($_POST['searchWord'])){
+			$searchWord = $_POST['searchWord'];
+			$sort = $_POST['sort'];
+			$item = $_POST['item'];
+			$result = DBsearch($searchWord, $item, $sort);
+         if(!is_null($result)){
+	         require_once("Company.php");
+	         $company = [];
+         	foreach($result as $row){
+            	$company[] = new Company($row['code'], $row['name'], $row['address'], $row['phone'], $row['labors']);
+         	}
+				echo "企業数：" . htmlspecialchars(count($company), ENT_QUOTES, 'UTF-8') . "件\n<br>\n";
+	         echo "<table>\n";
+         	echo "<tr>\n";
+      	   echo "<th>企業コード</th><th>企業名</th><th>所在地</th><th>電話番号</th><th>従業員数</th>\n";
+   	      echo "<tr>\n";
+	         foreach($company as $row){
+         	   echo "<tr>\n";
+      	      echo "<td>" . htmlspecialchars($row->getCode(), ENT_QUOTES, 'UTF-8') . "</td>\n";
+   	         echo "<td>" . htmlspecialchars($row->getName(), ENT_QUOTES, 'UTF-8') . "</td>\n";
+	            echo "<td>" . htmlspecialchars($row->getAddress(), ENT_QUOTES, 'UTF-8') . "</td>\n";
+            	echo "<td>" . htmlspecialchars($row->getPhone(), ENT_QUOTES, 'UTF-8') . "</td>\n";
+         	   echo "<td>" . htmlspecialchars($row->getLabors(), ENT_QUOTES, 'UTF-8') . "</td>\n";
+      	      echo "</tr>\n";
+   	      }
+	         echo "</table>\n";
+			}
+		}
+		else{
+			print("検索したい企業名を入力してください");
+		}
+		echo "<br>";
    ?>
 </body>
 </html>
+
+<?php
+   function DBsearch($searchWord, $item, $sort){
+      try{
+   	      $server = "jocalc1";
+      	   $DB = "r01_j4_exp";
+         	$portNumber = "5432";
+	         $userID = "td16131";
+   	      $userPass = "td16131";
+      	   $dbh = new PDO("pgsql:host=$server; dbname=$DB; port=$portNumber; user=$userID; password=$userPass");
+         	$sql = "SELECT * FROM company_21 WHERE name LIKE '%" . $searchWord . "%' ORDER BY " . $item . " " . $sort;
+				$stmt = $dbh->query($sql);
+      	   $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+         	$dbh = null;
+	      }
+   	   catch(Exception $e){
+   	   	$result = null;
+      	   print($e);
+	      }
+      return $result;
+   }
+?>
