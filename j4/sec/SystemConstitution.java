d8074be06bb4d9727e880ca470c81e2b3bf6a84f
notMayaku <mayakuzuki39@gmail.com>
2019-12-31 14:21:21 +0900

    ５台以上にも対応させ、台数の入力もできるようにした

--- j4/sec/SystemConstitution.java
+++ j4/sec/SystemConstitution.java
@@ -1,12 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class SystemConstitution{
    public static void main(String args[]){
-      ArrayList<String> list = new ArrayList<String>();
+      InputStreamReader isr = new InputStreamReader(System.in);
+      BufferedReader br = new BufferedReader(isr);
+      int num = 0;
+      String str;
 
-      list = con(4);
-      for(int i = 0; i < list.size(); i++){
-         System.out.println(list.get(i));
+      while(num == 0){
+         str = null;
+         System.out.print("台数 -> ");
+         try{
+            str = br.readLine();
+            try{
+               num = Integer.parseInt(str);
+            }
+            catch(NumberFormatException e){
+               num = 0;
+            }
+            if(num == 0){
+               System.out.println("入力エラーです。もう一度入力してください");
+            }
+         }
+         catch(IOException e){
+            e.printStackTrace();
+            num = 1;
+            break;
+         }
+      }
+
+      if(num > 1){
+         ArrayList<String> list = new ArrayList<String>();
+         list = con(num);
+         for(int i = 0; i < list.size(); i++){
+            System.out.println(list.get(i));
+         }
+      }
+      else{
+         System.out.println("R");
       }
    }
 
@@ -26,6 +60,7 @@ public class SystemConstitution{
 
    public static ArrayList<String> hoge(int x, int y){
       ArrayList<String> list = new ArrayList<String>();
+      ArrayList<String> work = new ArrayList<String>();
       ArrayList<String> xList = new ArrayList<String>();
       ArrayList<String> yList = new ArrayList<String>();
       
@@ -33,13 +68,25 @@ public class SystemConstitution{
          xList.add("R");
       }
       else{
-         xList = hoge(x-1, 1);
+         for(int i = 1; i < x; i++){
+            if(i > x-i) break;
+            else work = hoge(i, x-i);
+            for(int j = 0; j < work.size(); j++){
+               xList.add(work.get(j));
+            }
+         }
       }
       if(y == 1){
          yList.add("R");
       }
       else{
-         yList = hoge(y-1, 1);
+         for(int i = 1; i < y; i++){
+            if(i > y-i) break;
+            else work = hoge(i, y-i);
+            for(int j = 0; j < work.size(); j++){
+               yList.add(work.get(j));
+            }
+         }
       }
 
       if(x == 1 && y == 1){
